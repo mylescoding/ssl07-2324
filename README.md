32e7c1dbb31b0160036094a49f40c9fc787ee50f
Victor Jacob Manlapaz <manlapaz.victor.jacob@gmail.com>
2024-05-28 21:06:38 +0800

    updated repo structure, see README for quick guide.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ssl07-2324
-SSL07 Repository with Fixed Config Files
+SmartLight: A Decentralized MPC-based Multi-intersection Traffic Signal Optimization Algorithm
+
+GUIDE:
+
+
+To make simulation run with TraCI, make sure to include TraCI-integrated Python Code in the same directory as the SUMO Configuration files.
+
+Then run python file in terminal.
+
+
+For integrations with BoTorch, make sure to have conda environment.
---
+++ Working/performance_indicators.py
@@ -0,0 +1,45 @@
+import xml.etree.ElementTree as ET
+
+# Load and parse the XML file
+tree = ET.parse('tripinfo_000.xml')
+root = tree.getroot()
+
+tree2 = ET.parse('summary_out.xml')
+root2 = tree2.getroot()
+
+
+def extract_tripinfo(attribute_name):
+    values = []
+    for tripinfo in root.findall('tripinfo'):
+        value = tripinfo.get(attribute_name)
+        if value is not None:
+            values.append(float(value))
+    return sum(values)/len(values)
+
+def extract_summary(attribute_name):
+    values = []
+    for step in root2.findall('step'):
+        value = step.get(attribute_name)
+        if value is not None:
+            values.append(float(value))
+    return sum(values)/50400
+    #return sum(values)/50400
+
+
+attribute_qt = 'halting'
+average_qt = extract_summary(attribute_qt)
+print(f"Average queue length (veh): " + str(average_qt/40))
+
+attribute_wt = 'waitingTime'
+average_wt = extract_tripinfo(attribute_wt)
+
+print(f"Average time in queue (s): " + str(average_wt))
+
+attribute_vfr = 'running'
+average_vfr = extract_summary(attribute_vfr)
+print(f"Average vehicular flow rate (veh/hr): " + str (average_vfr))
+
+
+
+
+
